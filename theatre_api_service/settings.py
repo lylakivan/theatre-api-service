21c960b157c608c0ddfb15dbb75e946a9f35fa04
Lylak Ivan <lylakivanmate@gmail.com>
2024-01-25 15:21:59 +0200

    add README.md

--- theatre_api_service/settings.py
+++ theatre_api_service/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0en!o^7zb!qtw13=*qo^=(%z^_qj!_y3hg+d)24wn3bbc)j=72"
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
